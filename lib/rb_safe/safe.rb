a9a64a47769b043a6d3c6a18c86718ea3c1c3ad9
lord63 <lord63.j@gmail.com>
2015-12-29 21:41:20 +0800

    Add is_by_step method

--- lib/rb_safe/safe.rb
+++ lib/rb_safe/safe.rb
@@ -7,5 +7,13 @@ module RbSafe
       keyboard.any? { |pattern| pattern.include?(raw) or
                       pattern.include?(raw.reverse) }
     end
+
+    def self.is_by_step(raw)
+      delta = raw[1].ord - raw[0].ord
+      2.upto(raw.length-1) do |index|
+        return false if raw[index].ord - raw[index-1].ord != delta
+      end
+      return true
+    end
   end
 end
