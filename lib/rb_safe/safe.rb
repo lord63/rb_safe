525dc96835aa5bbce9ee4ef041c14bd6429fbb7f
lord63 <lord63.j@gmail.com>
2015-12-30 09:45:31 +0800

    Expose the method to the module

--- lib/rb_safe/safe.rb
+++ lib/rb_safe/safe.rb
@@ -1,19 +1,17 @@
 module RbSafe
-  class Safe
-    def self.is_asdf(raw)
-      # The current implementation treat those passwords that in the same line
-      # are "is asdf".
-      keyboard = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm']
-      keyboard.any? { |pattern| pattern.include?(raw) or
-                      pattern.include?(raw.reverse) }
-    end
+  def self.is_asdf(raw)
+    # The current implementation treat those passwords that in the same line
+    # are "is asdf".
+    keyboard = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm']
+    keyboard.any? { |pattern| pattern.include?(raw) or
+                    pattern.include?(raw.reverse) }
+  end
 
-    def self.is_by_step(raw)
-      delta = raw[1].ord - raw[0].ord
-      2.upto(raw.length-1) do |index|
-        return false if raw[index].ord - raw[index-1].ord != delta
-      end
-      return true
+  def self.is_by_step(raw)
+    delta = raw[1].ord - raw[0].ord
+    2.upto(raw.length-1) do |index|
+      return false if raw[index].ord - raw[index-1].ord != delta
     end
+    return true
   end
 end
