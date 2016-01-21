3c52ab15b1998fba464493cd72a9b57431c25f81
lord63 <lord63.j@gmail.com>
2016-01-21 10:30:01 +0800

    Add a simple test

--- test/rb_safe_test.rb
+++ test/rb_safe_test.rb
@@ -5,7 +5,42 @@ class RbSafeTest < Minitest::Test
     refute_nil ::RbSafe::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_asdf_password
+    is_asdf = RbSafe.check('asdfghjk')
+    assert is_asdf.message.include?('pattern')
+    not_asdf = RbSafe.check('asdfhjkl')
+    assert !not_asdf.message.include?('pattern')
+  end
+
+  def test_by_step_password
+    is_by_step = RbSafe.check('abcdefgh')
+    assert is_by_step.message.include?('pattern')
+    not_by_step = RbSafe.check('abcdfghi')
+    assert !not_by_step.message.include?('pattern')
+  end
+
+  def test_common_password
+    common_password = RbSafe.check('password')
+    assert common_password.message.include?('common')
+  end
+
+  def test_terrible_password
+    terrible_password = RbSafe.check(1)
+    assert_equal(terrible_password.strength, 'terrible')
+  end
+
+  def test_simple_password
+    simple_password = RbSafe.check('issafepassword')
+    assert_equal(simple_password.strength, 'simple')
+  end
+
+  def test_medium_password
+    medium_password = RbSafe.check('is.safe.password')
+    assert_equal(medium_password.strength, 'medium')
+  end
+
+  def test_strong_password
+    strong_password = RbSafe.check('is.safe.passw0rd?')
+    assert_equal(strong_password.strength, 'strong')
   end
 end
